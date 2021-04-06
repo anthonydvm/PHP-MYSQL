cb91cbc129590f16fac258092a0e74c8fba765b7
Lenovo <anthony.victoria95@gmail.com>
2021-04-06 11:40:45 -0500

    Entrando a la parte de operadores

--- Funciones y variables.php
+++ Funciones y variables.php
@@ -35,7 +35,33 @@
     echo str_word_count($mensaje);
     echo "<br>";
 
-    //Texto alrevez
-    echo str_rrev($mensaje);
+    //encontrar texto
+    echo strpos($mensaje, "programacion");
     echo "<br>";
+    
+    //Reeemplazar texto
+    echo str_replace("programacion", "electricidad", $mensaje);
+    echo "<br>";
+
+    //texto a minuscula
+    echo strtolower($mensaje);
+    echo "<br>";
+
+    //texto a mayuscula
+    echo strtoupper($mensaje);
+    echo "<br>";
+    
+    //comparar cadenas -1 si la 1 esta alfabeticamente mas lejos
+    echo strcmp("w", "a");
+    echo "<br>";
+
+    //imprimir de un caracter a otro
+    echo substr($mensaje, 10, 6);
+    echo "<br>";
+
+    //remover exceso de espacios en blanco
+    echo trim("             holaaa        anthony");
+    echo "<br>";
+
+
 ?>
