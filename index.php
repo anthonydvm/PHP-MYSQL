391bf44958ff3f1203a1ff5e48a1ba0c68f91dbc
Anthony Victoria <anthony.victoria95@gmail.com>
2021-04-05 22:38:33 -0500

    video #3

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php 
-    phpinfo();
+    
+    //DECLARACION DE VARIABLE
+    $variable = 35;
+    $nombre = "Anthony";
+
+    //imprimir variables
+
+    print "mi nombre es " .$nombre ."<br>";
+    echo "mi nombre es $nombre" ."<br>";
+
+    $num1 = 5;
+    $num2 = 10;
+
+    print "La resta es igual a: "."<br>";
+    echo $num2 - $num1. "<br>";
+
+    //arreglos
+    $nada = null;
+    $carros = array("Sedan", "Camioneta", "Camion");
+    $edad = array("Cincuenta", 50, "Diez", 10);
+
+    print_r ($carros);
+
 ?>
