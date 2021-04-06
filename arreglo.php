c548816075797275439f9ab194612cdaddea237b
Lenovo <anthony.victoria95@gmail.com>
2021-04-06 16:56:00 -0500

    continuar

---
+++ arreglo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+    body{
+        background:tomato;
+        text-align: center;
+    }
+    </style>
+</head>
+<body>
+    
+    <?php 
+
+    $frutas = array("manzana", "pera", "fresa");
+
+    print_r($frutas);
+
+    echo "<br>";
+
+    echo $frutas[1];
+
+    echo "<br>";
+
+    echo count($frutas) . "elementos";
+
+    echo "<br>";
+
+    ?>
+</body>
+</html>
