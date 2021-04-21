f5346ef0e853799244cb5cbbb92afc2303500304
Lenovo <anthony.victoria95@gmail.com>
2021-04-21 11:27:30 -0500

    Creacion de base de datos y de una tabla con sus columnas

--- 28creaciondebd.php
+++ 28creaciondebd.php
@@ -3,14 +3,17 @@
     $servidor = 'localhost';
     $username = 'root';
     $password = 'anthonydavid2';
+    $db = "todolistDB";
 
-    $conexion = new mysqli($servidor, $username, $password);
+    $conexion = new mysqli($servidor, $username, $password, $db);
 
-    if($conexion -> connect_error){
+    if($conexion-> connect_error){
         die("Se encontro el siguiente error: " . $conexion -> connect_error);
     }
 
-    $sql = "CREATE DATABASE todolistDB";
+    /* CREACI[ON DE DATA DE BASE
+   
+   $sql = "CREATE DATABASE todolistDB";
 
     if($conexion->query($sql) === true){
         echo "Base de datos creada correctamente...";
@@ -18,7 +21,20 @@
     else{
         die("Error al crear base de datos: " . $conexion->error);
     }
+    */
 
+    $sql = "CREATE TABLE todoTable(
+        id INT (11) AUTO_INCREMENT PRIMARY KEY,
+        texto VARCHAR (100) NOT NULL,
+        completado BOOLEAN NOT NULL,
+        timestamp TIMESTAMP
+    )";
+
+    if ($conexion->query($sql) === true){
+        echo "La tabla se creo correctamente";
+    }else{
+        die("Error al crear la tabla" . $conexion->error);
+    }
 
 
 ?>
